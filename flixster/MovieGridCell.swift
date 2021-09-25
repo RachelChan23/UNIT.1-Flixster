015fabcfd46c26b891bad4c0893946b40f24e405
Rachel Chan <87961019+RachelChan23@users.noreply.github.com>
2021-09-25 16:08:27 -0400

    Add files via upload

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+ //
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Rachel Chan on 9/25/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
